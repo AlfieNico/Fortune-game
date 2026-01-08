a342a011c8d6b32c48388135c8fe222d8fd39e37
AlfieNico <5fxnhmv982@privaterelay.appleid.com>
2026-01-08 22:24:33 +0000

    Update fortuneGame.py
    Added the stats

--- fortuneGame.py
+++ fortuneGame.py
@@ -12,9 +12,13 @@ xp = 0
 classType = "null"
 loads = -1
 
+
+
 #---------- Lists ----------
 
 classes = ["mage", "warrior", "tank"]
+statNames = ["health", "attackSpeed", "attackDamage", "intelligence", "mana", "luck", "agility", "defence"]
+statNumbers = ["20", "20", "20"]
 
 #---------- Subprograms ----------
 
@@ -36,38 +40,19 @@ def getFortune():
     fortune = fortunes[random.randint(0,4)]
     return fortune
 
-def playerAction(choice):
-    global coins
-    if choice.lower() == "actions":
-        print("possible actions are: getCoins, looseCoins, fortuneRoll, profile, quit.")
-        return False
-    elif choice.lower() == "getcoins":
-        coins += int(input("How many coins should be added: "))
-    elif choice.lower() == "loosecoins":
-        coins -= int(input("How many coins do you want to loose: "))
-    elif choice.lower() == "profile":
-        print(f"you are on {coins} coins.")
-        print(f"your level {level}")
-        print(f"xp untill next level {xp}")
-        print(f"your class is {classType}")
-    elif choice.lower() == "fortuneroll":
-        if coins >= 5:
-            fortune = getFortune()
-            print(fortune)
-        else:
-            print("This costs 5 coins")
-    elif choice.lower() == "quit":
-        save()
-        return True
-    return False
+def getClassStats():
+    global classType, statNumbers
+    with open("stats.json", "r") as f:
+        classStats = json.load(f)
+    statNumbers = classStats[classType]
 
 def save():
-    data = {"coins": coins,"level": level,"xp": xp,"classType": classType,"loads": loads}
+    data = {"coins": coins,"level": level,"xp": xp,"classType": classType,"loads": loads,"statNumbers": statNumbers}
     with open("save.json", "w") as f:
         json.dump(data, f)
 
 def load():
-    global coins, level, xp, classType, loads
+    global coins, level, xp, classType, loads, statNumbers
     if not os.path.exists("save.json") or os.stat("save.json").st_size == 0:
         return
     with open("save.json", "r") as f:
@@ -77,10 +62,11 @@ def load():
         xp = data.get("xp", xp)
         classType = data.get("classType", classType)
         loads = data.get("loads", loads)
+        statNumbers = data.get("statNumbers", statNumbers)
     loads += 1
 
 def classChose():
-    global classType
+    global classType, statNumbers
     classType = input("What class would you like to choose, type classes to see options: ")
     if classType.lower() == "classes":
         with open("classes.txt", "r") as f:
@@ -91,7 +77,53 @@ def classChose():
         if classType.lower() == "classes":
             with open("classes.txt", "r") as f:
                 print(f.read())
-            classType = input("What class would you like to choose: ")    
+            classType = input("What class would you like to choose: ")
+    getClassStats()
+
+def xpToNextLevel():
+    global level
+    return round(100 * (1.5 ** (level - 1))) - xp 
+
+def addXp(adding):
+    global xp, level
+    xpHold = xp + adding
+    while xpHold >= xpToNextLevel():
+        xpHold -= xpToNextLevel()
+        level += 1
+    xp = xpHold
+
+def playerAction(choice):
+    global coins, statNames, statNumbers
+    if choice.lower() == "actions":
+        print("possible actions are: getCoins, looseCoins, fortuneRoll, profile, stats, addXp, quit.")
+        return False
+    elif choice.lower() == "getcoins":
+        coins += int(input("How many coins should be added: "))
+    elif choice.lower() == "loosecoins":
+        coins -= int(input("How many coins do you want to loose: "))
+    elif choice.lower() == "profile":
+        print(f"you are on {coins} coins.")
+        print(f"your level {level}")
+        print(f"you on {xp} xp ")
+        print(f"{xpToNextLevel()} xp untill next level")
+        print(f"your class is {classType}")
+    elif choice.lower() == "fortuneroll":
+        if coins >= 5:
+            fortune = getFortune()
+            print(fortune)
+        else:
+            print("This costs 5 coins")
+    elif choice.lower() == "stats":
+        print("your stats are")
+        for stat in statNames:
+            print(f"{stat} : {statNumbers[stat]}")
+    elif choice.lower() == "addxp":
+        adding = int(input("enter: "))
+        addXp(adding)
+    elif choice.lower() == "quit":
+        save()
+        return True
+    return False
 
 def main():
     global classType
